90cb5d18a10b729f4884e9fa260db055248012a8
lab6TranTrungKien <19130110@st.hcmuaf.edu.vn>
2022-03-23 11:13:54 +0700

    first commit

---
+++ assets/js/ToggleIcon.js
@@ -0,0 +1,6 @@
+let toggleIcon = document.querySelector('.toggle')
+let circularMain =document.querySelector('.circular-main')
+
+toggleIcon.onclick =function(){
+    circularMain.classList.toggle('active')
+}
